778a9986611bc5514477c1d268ba22f6012dfd21
Wes Breisch <wabreisch@quinnipiac.edu>
2015-09-12 17:24:11 -0400

    Adds Euclid's algorithm

--- euclidsAlgorithm/euclid.c
+++ euclidsAlgorithm/euclid.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int euclid(int a, int b);
 
-int main() {
-  int a=28;
-  int b=392;
+int main(int argc, char** argv) {
+  if (argc != 3) { 
+    printf("Error: please provide two command line arguments.\n");
+    printf("Example: a.out 123 832\n");
+    return 1; // return error
+  }
+  int a=atoi(argv[1]);
+  int b=atoi(argv[2]);
   printf("gcd of %d and %d: %d\n", a, b, euclid(a,b));
 	return 0;
 }
 
-// gcd(a,b) = gcd(a, b % a) where a < b
+// gcd(a,b) = gcd(a, b % a) b
 int euclid(int a, int b) {
-  if (a > b) { b = a }
-  if (a != 0 && b!= 0) {
-
-  }
-  return 4;
+  if (b == 0) { return a; }
+  else { return euclid(b, a%b); }
 }
